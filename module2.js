8736655caba8ae14b466e6d381fddcc60eaa79db
Yulia503 <125694915+Yulia503@users.noreply.github.com>
2023-07-19 22:39:26 +0300

    Update module2.js

--- module2.js
+++ module2.js
@@ -119,7 +119,38 @@
 // const result = arr.join(' ')
 // console.log(result);
 
-const string = 'Welcome to the future'
-const result = string.split(' ').reverse().join(' ')
-console.log(result);
+// const string = 'Welcome to the future'
+// const result = string.split(' ').reverse().join(' ')
+// console.log(result);
+
+/**
+ * 
+//  * @param {*} params 
+//  */
+
+
+
+// function
+//     name(params) {
+// }
+
+// function checkAge(age) {
+// if (age >= 18) {
+//     return 'You are an adult'
+//     } 
+//     return "You are a minor"
+// }
+//  console.log(checkAge(20));
+// console.log(checkAge(8) );
+
+// function checkPassword(password) {
+//     const ADMIN_PASSWORD = 'jqueryismyjam'
+//     if (password === ADMIN_PASSWORD) {
+//         return 'Welcome'
+//     }
+//         return "Access denied, wrong password!"
+// }
+// console.log(checkPassword("polyhax"));
+// console.log(checkPassword("jqueryismyjam"));
+
 
