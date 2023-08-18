86cb814bcbc8e5ecfe93e16c351a2eed9766c56e
Yulia503 <125694915+Yulia503@users.noreply.github.com>
2023-08-18 21:06:04 +0300

    Update module3.js

--- module3.js
+++ module3.js
@@ -820,3 +820,21 @@
    
 //   },
 // };
+
+const book = {
+  title: "The Last Kingdom",
+  author: "Bernard Cornwell",
+  genres: ["historical prose", "adventure"],
+  isPublic: true,
+  rating: 8.38,
+};
+
+const bookTitle = book["title"];
+console.log(bookTitle); // 'The Last Kingdom'
+
+const bookGenres = book["genres"];
+console.log(bookGenres); // ['historical prose', 'adventurs']
+
+const propKey = "author";
+const bookAuthor = book[propKey];
+console.log(bookAuthor); // 'Bernard Cornwell'
